6c43eb83442e22049772aa7e886ffaa834e149db
alfredchli <chli.alfred@gmail.com>
2017-05-04 17:24:11 +0800

    Add files via upload

--- ireserve/script.js
+++ ireserve/script.js
@@ -1,3 +1,17 @@
+$( document ).ready(function() {
+$( "#login_submit" ).bind( "click", function() {
+  $( "login-pane" ).hide();
+  $( "admin-pane" ).show();
+});
+
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+});
+
+function onLoginSubmit() {
+	alert(document.getElementByName('username').text);
+    document.getElementById('login-pane').style = 'display:none';
+	document.getElementById('admin-pane').style = 'display:block';
+    return false;
+}
